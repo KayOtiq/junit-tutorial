1bc5eaa695ae38efbb81b658d74bc1ac4d7e6b3b
Kay Otiq <lyndamontanez@gmail.com>
2019-10-26 21:24:03 -0700

    added ComputeCircleRadius method and unit test

--- src/main/java/io/kotiq/MathUtils.java
+++ src/main/java/io/kotiq/MathUtils.java
@@ -5,5 +5,13 @@ public class MathUtils {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	
+	public double computeCircleArea (double radius) {
+		//1.  return 0; - fail
+		//2.  returm 3.14 * radius * radius; - fail
+		return Math.PI * radius * radius; //Pass!
+	
+		
+	}
 
 }
--- src/test/java/io/kotiq/MathUtilsTest.java
+++ src/test/java/io/kotiq/MathUtilsTest.java
@@ -7,9 +7,34 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
-		System.out.println("test has run!");
-		//fail("not yet implemented");
+	void testAdd() {
+		//create new instance
+		MathUtils mathUtils = new MathUtils();
+		
+		int expected = 2;
+		int actual = mathUtils.add(1, 1); 
+		assertEquals(expected,actual, "Add method should add two number and return correct results"); //message when the test fails
 	}
+	
+	@Test
+	void testAddFail() {
+		//create new instance
+		MathUtils mathUtils = new MathUtils();
+		
+		int expected = 1;
+		int actual = mathUtils.add(1, 1); 
+		assertEquals(expected,actual, "Add method adds 2 numbers and validates does not return incorrect results"); //message when the test fails
+	}
+	
+	@Test
+	void testComputeCircleRadius() {
+		//create new instance
+		MathUtils mathUtils = new MathUtils(); //why are we calling this more than once?
+		
+		//int expected = 10;
+		//int actual = 314.1592653589; //removing unnecessary calls
+		assertEquals(314.1592653589793,mathUtils.computeCircleArea(10), "Didn't correctly calc area of circle"); //message when the test fails
+	}
+
 
 }
